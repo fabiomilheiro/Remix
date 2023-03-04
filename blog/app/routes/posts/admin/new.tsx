cd4a1e216c195ab43be0ab97c4510b0db9c8a053
Fabio Milheiro <fabiomilheiro@gmail.com>
2023-03-04 22:24:17 +0000

    Update new.tsx

--- blog/app/routes/posts/admin/new.tsx
+++ blog/app/routes/posts/admin/new.tsx
@@ -1,11 +1,13 @@
 import type { ActionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
 import { createPost } from "~/models/post.server";
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export const action = async ({ request }: ActionArgs) => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+
   const formData = await request.formData();
 
   const post = {
@@ -31,6 +33,7 @@ export const action = async ({ request }: ActionArgs) => {
 
 export default function NewPost() {
   const errors = useActionData<typeof action>();
+  const navigation = useNavigation();
 
   return (
     <Form method="post">
@@ -64,6 +67,7 @@ export default function NewPost() {
       <p className="text-right">
         <button
           type="submit"
+          disabled={navigation.state == "submitting"}
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
         >
           Create Post
